e777d327b02676d67784db0a2454a26488393aec
Brandon <brandonding21@gmail.com>
2025-07-24 00:08:28 -0400

    sdoifjasodfj

--- src/kotlin.kt
+++ src/kotlin.kt
@@ -2,17 +2,25 @@ import kotlin.times
 
 fun main() {
     println("Hi welcome to awesome program. Today we will be learning Taxes!!!!!")
+    println("DISCLAIMER: THIS IS NOT TURBOTAX")
+    println("DISCLAIMER: THIS IS NOT TURBOTAX")
+    println("DISCLAIMER: THIS IS NOT TURBOTAX")
+    println("DISCLAIMER: THIS IS NOT TURBOTAX")
+    Thread.sleep(1_000)
+    println("DISCLAIMER: THIS IS NOT TURBOTAX")
+    println("DISCLAIMER: THIS IS NOT TURBOTAX")
+    println("DISCLAIMER: THIS IS NOT TURBOTAX")
     Thread.sleep(1_000)
     println("What's your name?")
     val userInput = readln()
     println("Hello, $userInput")
-    println("what is your tax (more specially incoem)??? (yearly))")
+    println("What's your annual income")
     val annualIncome = readln().toDouble()
-    println("what's the tax rates?")
+    println("What's your tax rate in percent?")
     val taxRatePercent = readln().toDouble()
-    println("So, you make $annualIncome per year and the taxes are $taxRatePercent.")
+    println("So, you make $$annualIncome per year and the tax rates are $taxRatePercent%.")
     val taxRate = taxRatePercent / 100
     val taxOwed = annualIncome * taxRate
-    println("You owe $taxOwed in taxes this year. Have fun!!!!!!!!!!!!!!")
+    println("You owe $$taxOwed in taxes this year. Have fun!!!!!!!!!!!!!!")
 }
 
